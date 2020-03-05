33d7a0f11522c965e0d4d27b4a13b1aed99535d9
donfyy <xinxin.wang@pintec.com>
2020-03-05 18:27:33 +0800

    lateinit

--- src/main/kotlin/p1/P1.kt
+++ src/main/kotlin/p1/P1.kt
@@ -2,7 +2,15 @@ package com.donfyy.kotlin.p1
 
 const val B_CONST = 1
 val constB = 1
+get() {
+    return field
+}
 val c = constB + B_CONST
+get() = field
+
+lateinit var v3 : String;
+//lateinit lateinit可用于顶层属性/属性/局部变量
+//lateinit修饰的属性或变量不用判空，不知道什么时候会被初始化，初始化时间是在声明之后
 //可以有全局变量，有点像c/c++
 fun main() {
 //    com.donfyy.kotlin.B_CONST
