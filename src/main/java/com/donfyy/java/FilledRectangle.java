02f1a85c439b0f6a6fdb70bc4b6a4231c27d7710
donfyy <xinxin.wang@pintec.com>
2020-03-09 14:05:09 +0800

    add more

--- src/main/java/com/donfyy/java/FilledRectangle.java
+++ src/main/java/com/donfyy/java/FilledRectangle.java
@@ -3,4 +3,13 @@ package com.donfyy.java;
 //private static final int ABC=902;没有顶层属性，没有顶层函数，没有顶层常量
 public class FilledRectangle extends Rectangle implements Polygon {
     // draw 方法不必重写
+
+    public static void m1() {
+    }
+
+    public static void main(String... args) {
+        FilledRectangle.m1();//可以使用子类类名调用父类静态方法
+
+        Rectangle.m1();
+    }
 }
--- src/main/java/com/donfyy/java/Rectangle.java
+++ src/main/java/com/donfyy/java/Rectangle.java
@@ -3,4 +3,7 @@ package com.donfyy.java;
 public class Rectangle {
     public void draw() {
     }
+
+    public static void m1() {
+    }
 }
