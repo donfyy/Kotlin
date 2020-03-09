84a53bfa583550caa3b3fe34fbac6bd16058ca3f
donfyy <xinxin.wang@pintec.com>
2020-03-09 11:22:31 +0800

    add more

--- src/main/kotlin/Accessbility.kt
+++ src/main/kotlin/Accessbility.kt
@@ -1,6 +1,15 @@
 package com.donfyy.kotlin
 
-class Outer {
+open class B {
+    private fun f1() {
+
+    }
+
+    private fun f2() {
+
+    }
+}
+class Outer : B(){
     class Inter {
         var p1: String = ""
     }
@@ -14,4 +23,19 @@ class Outer {
 
         var p11 = Inner().p1
     }
+}
+
+fun Outer.f1() {
+    f1()
+}
+
+fun Outer.f2() {
+//    super.f2()
+    this.f2()
+}
+//扩展函数与成员函数同名，若成员函数可被访问则优先使用成员函数。
+
+
+fun main() {
+    Outer().f1()
 }
