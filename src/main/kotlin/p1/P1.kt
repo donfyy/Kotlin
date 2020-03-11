a4eee5f2608de738f5890cf0c5ecd4e3cf0171bc
donfyy <xinxin.wang@pintec.com>
2020-03-11 18:10:29 +0800

    add more

--- src/main/kotlin/p1/P1.kt
+++ src/main/kotlin/p1/P1.kt
@@ -4,30 +4,41 @@ import kotlin.reflect.jvm.internal.impl.renderer.KeywordStringsGenerated
 
 const val B_CONST = 1
 val constB = 1
-get() {
-    return field
-}
+    get() {
+        return field
+    }
 val c = constB + B_CONST
-get() = field
+    get() = field
 
-lateinit var v3 : String;
+lateinit var v3: String;
 
 open class B {
 
 }
+
 //超类型列表里，最多只能有一个类，但可以有多个接口，和Java一致
-class C :A(){
+class C : A() {
 
 }
 
 open class A {
-    lateinit var v1 : B
+    lateinit var v1: B
+    private val v2: String = ""
 
     fun fun1() {
 //        var initialized = v1.isInitialized
         var javaClass = v1?.javaClass
     }
+
+    private fun fun2() {
+
+    }
 }
+
+fun A.extFun1() {
+//    this.v2   不能在类外部访问类的私有成员
+}
+
 //lateinit var v1 : Int; 不能用在基本类型变量上
 //lateinit lateinit可用于顶层属性/属性/局部变量
 //lateinit修饰的属性或变量不用判空，不知道什么时候会被初始化，初始化时间是在声明之后
